8f5521de4ffebe0cc1bc87dd1f8365a5d2962f66
KleanthisLiontis <kleanthisliontis@gmail.com>
2023-10-03 17:15:57 +0300

    still on sertverr

--- src/server.rs
+++ src/server.rs
@@ -14,8 +14,24 @@ impl Server {
 
     //We can make it &mut self or &self so we can change ownership
     pub fn run (self) {
-         println!("Server running and listening on {}.", self.address);
 
-         let tcp_listener = TcpListener::bind(&self.address).unwrap();
+        println!("Server running and listening on {}.", self.address);
+        let tcp_listener = TcpListener::bind(&self.address).unwrap();
+        
+        loop{
+            match tcp_listener.accept() {
+                //Can ignore tupple/or each variable returned from ok wrapper by using _ instread of tuple.
+                Ok((stream,address)) => {
+                    println!("Connections accepted!")
+                },
+                Err(e) => println!("Error accepting connections! {}", e),
+            }
+
+            let result = tcp_listener.accept();
+            if(result.is_err()) {
+                continue;
+            }
+            let (stream,address) = result.unwrap();
+        };
     }
 }
